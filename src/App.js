da5d947a393e6907bd8d67dbb11aa6e9bd45e022
Luis Manuel <luismanuelestradarosas@gmail.com>
2023-09-18 17:21:44 -0600

    primer commit del curso de react

---
+++ src/App.js
@@ -0,0 +1,24 @@
+import logo from './platzi.webp';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+
+   
+    </div>
+  );
+}
+
+function TodoItem() {
+  return(
+    <li>
+      <spam>v</spam>
+      <p>Llorar y chillar</p>
+      <spam>X</spam>
+    </li>
+  );
+
+}
+
+export default App;
